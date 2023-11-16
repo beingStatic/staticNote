98e8be468a71f401dc84bae63833f9e051728a5b
beingStatic <mohammedsufiyan23@gmail.com>
2023-11-16 23:56:12 +0530

    Setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button className='bg-red-500'>Never Settle</Button>
+    </div>
+  )
+}
+
